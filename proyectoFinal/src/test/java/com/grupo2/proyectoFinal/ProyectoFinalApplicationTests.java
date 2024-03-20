32c5c87a40d5ccacd5a0bce5aefa887a0d32d9b8
nicolas-cossio <nicolas.cossio@pucp.edu.pe>
2024-03-19 21:12:12 -0500

    Creacion proyecto spring

---
+++ proyectoFinal/src/test/java/com/grupo2/proyectoFinal/ProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2.proyectoFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
